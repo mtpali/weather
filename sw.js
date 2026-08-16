a253419ed361f2ce28552475963b15f05d9aef77
MTPALI <alimtp@atomicmail.io>
2026-08-16 09:27:17 +0330

    chore: improve offline cache for redesigned app

--- sw.js
+++ sw.js
@@ -1,12 +1,47 @@
-const CACHE='iran-weather-v1';
-const APP=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+const CACHE='iran-weather-v2';
+const APP=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(APP))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch',event=>{
   const req=event.request;
-  if(req.method!=='GET')return;
+  if(req.method!=='GET') return;
   const url=new URL(req.url);
+
   if(url.origin===self.location.origin){
-    event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html'))));
+    event.respondWith(
+      fetch(req)
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req,copy));
+          return res;
+        })
+        .catch(()=>caches.match(req).then(hit=>hit||caches.match('./index.html')))
+    );
+    return;
+  }
+
+  if(url.hostname==='fonts.googleapis.com'||url.hostname==='fonts.gstatic.com'){
+    event.respondWith(
+      caches.match(req).then(hit=>hit||fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+        return res;
+      }))
+    );
   }
 });
