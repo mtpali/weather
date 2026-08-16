c44b6a7da735b274329324e199d2f58a486c78e0
MTPALI <alimtp@atomicmail.io>
2026-08-16 09:43:28 +0330

    android: use built-in Kotlin with Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -39,10 +38,6 @@ android {
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {
